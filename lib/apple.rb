2bcf898033cea7d6e2594d30c2b73cd163593d88
Tyler Machen <tylermachen@gmail.com>
2015-07-31 00:17:03 -0400

    complete apple holiday methods

--- lib/apple.rb
+++ lib/apple.rb
@@ -1,12 +1,7 @@
 def apple_picker_with_select(fruit_list)
-  # fruit_list will look something like: ["apple", "banana", "apple"]
-  # you want to return an array that just has apples in it
-  
+  fruit_list.select { |f| f == "apple" }
 end
 
 def apple_picker_with_collect(fruit_list)
-  # fruit_list will look something like: ["apple", "banana", "apple"]
-  # you want to return an array that just the apples, just like the 
-  # challenge above
-  
+  fruit_list.collect { |f| f if f == "apple" }.compact
 end
